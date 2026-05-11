e65d1bc256cbb8eb58cb80e7cf2cfd4015e07223
diraayudiaa <ayudiamaharani77@gmail.com>
2026-05-11 08:22:28 +0700

    routes

--- perpustakaan/routes/api.php
+++ perpustakaan/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\Auth\LoginUserController;
 use App\Http\Controllers\API\Auth\LoginAdminController;
 use App\Http\Controllers\API\Auth\RegisterAdminController;
+use App\Http\Controllers\API\BukuController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -23,3 +24,12 @@ Route::post('/login-admin', [LoginAdminController::class, 'login']);
 // ================= REGISTER ADMIN =================
 
 Route::post('/register-admin', [RegisterAdminController::class, 'register']);
+
+// ================= BUKU =================
+Route::get('/bukus', [BukuController::class, 'index']);
+Route::get('/bukus/search', [BukuController::class, 'search']);
+Route::get('/bukus/{id}', [BukuController::class, 'show']);
+
+Route::post('/bukus', [BukuController::class, 'store']);
+Route::put('/bukus/{id}', [BukuController::class, 'update']);
+Route::delete('/bukus/{id}', [BukuController::class, 'destroy']);
