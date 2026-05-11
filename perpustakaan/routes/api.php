2b287f1ac660fb9e6a0586f365051ccd4673a049
Alya Hamidah <lyahamidah14@gmail.com>
2026-05-11 08:20:14 +0700

    routes

--- perpustakaan/routes/api.php
+++ perpustakaan/routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\Auth\LoginUserController;
 use App\Http\Controllers\API\Auth\LoginAdminController;
 use App\Http\Controllers\API\Auth\RegisterAdminController;
+use App\Http\Controllers\API\RiwayatController;
+use App\Http\Controllers\API\PengembalianController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -23,3 +25,7 @@ Route::post('/login-admin', [LoginAdminController::class, 'login']);
 // ================= REGISTER ADMIN =================
 
 Route::post('/register-admin', [RegisterAdminController::class, 'register']);
+
+// ================= Riwayat dan pengembalian =================
+Route::get('/riwayat/{id_user}', [RiwayatController::class, 'index']);
+Route::put('/pengembalian/{id}', [PengembalianController::class, 'kembalikan']);
